bd3e98fae58b1ce93acc0b58884b16fa3be0ff26
HappyTommyFriends <happytommyfriends@gmail.com>
2022-11-29 11:56:56 -0800

    Pause movement during opening shovel animation

--- Assets/scripts/Controller Sets/Ray/WHRActionController.cs
+++ Assets/scripts/Controller Sets/Ray/WHRActionController.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class WHRActionController : ActionController
 {
+	public float openingAnimationDuration = 4.0f;
+	protected bool frozen = false;
+
+	void Start() {
+		Freeze();
+		Invoke("Unfreeze", openingAnimationDuration);
+	}
+
+	void Freeze() {
+		frozen = true;
+	}
+
+	void Unfreeze() {
+		frozen = false;
+	}
+
 	public override string establishAction(string intent) {
+		if(frozen)
+			return "idle";
+
 		switch(intent) {
 			case "none":
 				return "idle";
