f007e251611534e0f62271bb6a9eccce48f6a005
HappyTommyFriends <happytommyfriends@gmail.com>
2022-11-29 13:05:34 -0800

    Pause and noise before boss

--- Assets/scripts/Scenes/CaveTrackCoordinator.cs
+++ Assets/scripts/Scenes/CaveTrackCoordinator.cs
@@ -9,29 +9,34 @@ public class CaveTrackCoordinator : MonoBehaviour
 	public WHRPlayerController player;
 	public AudioClip mainTrack;
 	public AudioClip caveTrack;
+	public AudioClip bossIntro;
 	public AudioClip bossTrack;
 
 	public float caveCutoff;
 	public float bossCutoff;
+	public float bossIntroDuration = 3.0f;
 
 	public Scorpion scorpion;
 
 	public AudioSource source;
 	int state = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-			instance = this;
-			source = GetComponent<AudioSource>();
-      playTrack(mainTrack);
-    }
+	private IEnumerator coroutine;
 
-    // Update is called once per frame
-    void Update()
-    {
-		if(state == 3)
+  // Start is called before the first frame update
+  void Start()
+  {
+		instance = this;
+		source = GetComponent<AudioSource>();
+    playTrack(mainTrack);
+  }
+
+  // Update is called once per frame
+  void Update() {
+		if(state == 4)
 			return;
+
+			Debug.Log("State: " + state);
 
 		if(state == 0) {
 			if(player.transform.position.y < caveCutoff) {
@@ -42,18 +47,31 @@ public class CaveTrackCoordinator : MonoBehaviour
 		}
 
 		if(state == 1 && player.transform.position.x > bossCutoff) {
-			playTrack(bossTrack);
 			state = 2;
-			// This doesn't belong here but cutting some corners in crunch time.
-			scorpion.activate();
+			Time.timeScale = 0;
+			playTrack(bossIntro);
+			coroutine = BossTime(bossIntroDuration);
+      StartCoroutine(coroutine);
 			return;
 		}
 
-		if(state == 2 && scorpion.isDead()) {
+		if(state == 3 && scorpion.isDead()) {
 			playTrack(caveTrack);
-			state = 3;
+			state = 4;
 		}
-    }
+  }
+
+	private IEnumerator BossTime(float waitTime) {
+		yield return new WaitForSecondsRealtime(waitTime);
+		Debug.Log("BossTime");
+		Time.timeScale = 1f;
+			Debug.Log("BossTime post timeScale");
+		playTrack(bossTrack);
+		state = 3;
+		// This doesn't belong here but cutting some corners in crunch time.
+		scorpion.activate();
+			Debug.Log("BossTime end");
+	}
 
 	void playTrack(AudioClip clip) {
 		source.clip = clip;
