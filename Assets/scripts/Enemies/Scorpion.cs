7ea7ee314dcc109a976c959f51a054834f625a0a
tmest <thomas.meston@gmail.com>
2023-01-07 22:57:24 -0800

    Many level design tweaks to address bugs
    1. Fixed menu title animation
    2. Adjusted level one platforms to allow player to get out of places. Shortened the level slightly by moving the diner closer.
    3. Added enemies to level 1
    4. Tweaked the timing of score counting on the score screen (made it faster)'
    5. Changed the dialogue of Joe in the diner to say "old well" instead of oasis.
    6. Removed rock gaps in the digging section to eliminate possibility of getting stuck.
    7. Tweaked the death function of the boss scorpion to move out of the way when killed. (not yet fixed)

--- Assets/scripts/Enemies/Scorpion.cs
+++ Assets/scripts/Enemies/Scorpion.cs
@@ -8,19 +8,19 @@ public class Scorpion : MonoBehaviour
 	public GameManager gameManager;
 	public float walkSpeed = 1f;
 	public float chargeSpeed = 1.3f;
-	public float attackRange = 0.16f;	
+	public float attackRange = 0.16f;
 	public float hp = 8;
 	public WHRPlayerController player;
-	
+
 	public AudioClip clawAttack;
 	public AudioClip tailAttack;
 	public AudioClip jab;
 	public AudioClip charge;
 	public AudioClip shuffleNoise;
-	
+
 	protected Animator animator;
 	protected Rigidbody2D rigidBody;
-	
+
 	float horizontalSpeed;
 	int state;
 	bool smoothMoving = true;
@@ -33,7 +33,7 @@ public class Scorpion : MonoBehaviour
 	bool motionFrozen = false;
 	bool dead = false;
 	bool active = false;
-	
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,23 +43,23 @@ public class Scorpion : MonoBehaviour
 		// Invoke("die", 1.2f);
 		// Invoke("InvokePattern", 4f);
     }
-	
+
 	public void activate() {
 		active = true;
 		InvokePattern();
 	}
-	
+
 	void free() {
 		freeStyle = true;
 		smoothMoving = true;
 		setState(0);
 	}
-	
+
 	void InvokePattern() {
 		free();
 		Invoke("InvokePattern2", 8f);
 	}
-	
+
 	void InvokePattern2() {
 		if(playerOnTop()) {
 			tailStrike();
@@ -80,7 +80,7 @@ public class Scorpion : MonoBehaviour
 			// Invoke("InvokePattern2", 18f);
 		}
 	}
-	
+
 	void attack1() {
 		freeStyle = false;
 		Debug.Log("attack1");
@@ -91,47 +91,47 @@ public class Scorpion : MonoBehaviour
 		}
 		nextLeft = !nextLeft;
 	}
-	
+
 	void attack1Left() {
 		leftJab();
 		Invoke("idle", 0.5f);
 		Invoke("leftSnap", 1f);
 		Invoke("idle", 1.6f);
 	}
-	
+
 	void attack1Right() {
 		rightJab();
 		Invoke("idle", 0.5f);
 		Invoke("rightSnap", 1f);
 		Invoke("idle", 1.6f);
 	}
-	
+
 	float attack1Time() {
 		return 1.65f;
 	}
-	
+
 	void leftJab() {
 		playSound(jab);
 		setState(24);
 	}
-	
+
 	void leftSnap() {
 		playSound(clawAttack);
 		setState(34);
 		attackFacing(2);
 	}
-	
+
 	void rightJab() {
 		playSound(jab);
 		setState(22);
 	}
-	
+
 	void rightSnap() {
 		playSound(clawAttack);
 		setState(32);
 		attackFacing(2);
 	}
-	
+
 	void chargeAttack() {
 		Debug.Log("chargeAttack");
 		freeStyle = false;
@@ -139,11 +139,11 @@ public class Scorpion : MonoBehaviour
 		horizontalSpeed = -chargeSpeed;
 		setState(50);
 	}
-	
+
 	float chargeAttackTime() {
 		return 1.9f;
 	}
-	
+
 	void backup() {
 		Debug.Log("backup");
 		setState(0);
@@ -151,14 +151,14 @@ public class Scorpion : MonoBehaviour
 		smoothMoving = true;
 		horizontalSpeed = walkSpeed;
 	}
-	
+
 	void idle() {
 		freeStyle = false;
 		setState(0);
 		horizontalSpeed = 0;
 		smoothMoving = true;
 	}
-	
+
 	void attack2() {
 		freeStyle = false;
 		Debug.Log("attack2");
@@ -167,29 +167,29 @@ public class Scorpion : MonoBehaviour
 		rigidBody.AddForce(new Vector2(-5000f, 7500f));
 		Invoke("attack2b", 0.5f);
 	}
-	
+
 	void attack2b() {
 		setState(24);
 		Invoke("idle", 0.5f);
 		Invoke("attack2b2", 1.0f);
 	}
-	
+
 	void attack2b2() {
 		leftSnap();
 		Invoke("idle", 0.5f);
 	}
-	
+
 	void tailStrike() {
 		freeStyle = false;
 		Debug.Log("tailStrike");
 		Invoke("shuffle", shuffleTime);
 		Invoke("actualTailStrike", shuffleTime * 4 + tailStrikeWait);
 	}
-	
+
 	float tailStrikeTime() {
 		return shuffleTime * 4 + tailStrikeWait + 0.5f;
 	}
-	
+
 	void actualTailStrike() {
 		playSound(tailAttack);
 		setState(33);
@@ -199,30 +199,30 @@ public class Scorpion : MonoBehaviour
 			hitPlayer(4);
 		}
 	}
-	
+
 	void attackFacing(float damage) {
 		if(playerInAttackZone())
 			player.damage(damage);
 	}
-	
+
 	bool playerInAttackZone() {
 		if(player.transform.position.y > -4.57)
 			return false;
-		
+
 		if(player.transform.position.x < transform.position.x)
 			return (player.transform.position.x > transform.position.x - width / 2 - attackRange);
-		
+
 		return (player.transform.position.x < transform.position.x + width / 2 + attackRange);
 	}
-	
+
 	void hitPlayer(float amount) {
 		player.damage(amount);
 	}
-	
+
 	bool playerOnTop() {
 		return (player.transform.position.y > transform.position.y && Math.Abs(player.transform.position.x - transform.position.x) < 0.5f);
 	}
-	
+
 	void shuffle() {
 		playSound(shuffleNoise);
 		setState(0);
@@ -231,61 +231,61 @@ public class Scorpion : MonoBehaviour
 		transform.position = new Vector3(transform.position.x + shuffleSize, transform.position.y, transform.position.z);
 		Invoke("shuffle2", shuffleTime);
 	}
-	
+
 	void shuffle2() {
 		transform.position = new Vector3(transform.position.x - shuffleSize, transform.position.y, transform.position.z);
 		Invoke("shuffle3", shuffleTime);
 	}
-	
+
 	void shuffle3() {
 		transform.position = new Vector3(transform.position.x + shuffleSize, transform.position.y, transform.position.z);
 		Invoke("shuffle4", shuffleTime);
 	}
-	
+
 	void shuffle4() {
 		transform.position = new Vector3(transform.position.x - shuffleSize, transform.position.y, transform.position.z);
 	}
-	
+
 	void setState(int state) {
 		animator.SetInteger("state", state);
 		this.state = state;
 	}
-	
+
 	void moveLeft() {
 		faceLeft();
 		horizontalSpeed = -walkSpeed;
 		setState(0);
 	}
-	
+
 	void moveRight() {
 		faceRight();
 		horizontalSpeed = walkSpeed;
 		setState(0);
 	}
-	
+
 	void faceLeft() {
 		// GetComponent<SpriteRenderer>().flipX = false;
 		transform.localRotation = Quaternion.Euler(0, 0, 0);
 	}
-	
+
 	void faceRight() {
 		// GetComponent<SpriteRenderer>().flipX = true;
 		transform.localRotation = Quaternion.Euler(0, 180, 0);
 	}
-	
+
 	void freezeMovement() {
 		motionFrozen = true;
 	}
-	
+
 	void freezeMovementFor(float duration) {
 		motionFrozen = true;
 		Invoke("unfreezeMovement", duration);
 	}
-	
+
 	void unfreezeMovement() {
 		motionFrozen = false;
 	}
-	
+
 	void frozenAttack1() {
 		Invoke("freezeMovement", 0.5f);
 		attack1();
@@ -297,7 +297,7 @@ public class Scorpion : MonoBehaviour
     {
 		if(!active || dead)
 			return;
-		
+
 		if(freeStyle) {
 			if(playerOnTop()) {
 				tailStrike();
@@ -317,42 +317,42 @@ public class Scorpion : MonoBehaviour
 		if(!motionFrozen && smoothMoving)
 			rigidBody.velocity = new Vector2(horizontalSpeed, 0);
     }
-	
+
 	public void damage(float amount = 1f) {
 		Debug.Log("Scorpion.damage()...");
 		hp -= amount;
 		if(hp <= 0)
 			die();
 	}
-	
+
 	void die() {
 		death();
 		gameManager.addScore(1000);
 	}
-	
+
 	// Kill NOT as the player (no score bonus)
 	public void death() {
 		CancelInvoke();
 		dead = true;
 		active = false;
-		transform.position = new Vector3(transform.position.x, transform.position.y - 0.8f, transform.position.z);
-		transform.localRotation = Quaternion.Euler(180, 0, 0);
-		// Destroy(GetComponent<BoxCollider2D>());
-		// Destroy(GetComponent<BoxCollider2D>());
+		transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+		transform.localRotation = Quaternion.Euler(0, 0, 180);
+		Destroy(GetComponent<BoxCollider2D>());
+		Destroy(GetComponent<BoxCollider2D>());
 		Component[] hingeJoints;
 
         hingeJoints = GetComponents(typeof(BoxCollider2D));
 
         foreach (BoxCollider2D joint in hingeJoints)
             Destroy(joint);
-			
+
 		rigidBody.AddForce(new Vector2(0, 750f));
 	}
-	
+
 	void playSound(AudioClip clip) {
 		GetComponent<AudioSource>().PlayOneShot(clip);
 	}
-	
+
 	public bool isDead() {
 		return dead;
 	}
